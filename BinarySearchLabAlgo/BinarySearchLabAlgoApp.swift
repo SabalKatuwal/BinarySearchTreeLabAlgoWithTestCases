a0dfb89c21c1f2cacda602a9b2a59bc8a3468bfa
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-05-02 19:40:29 +0545

    Initial Commit

---
+++ BinarySearchLabAlgo/BinarySearchLabAlgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinarySearchLabAlgoApp.swift
+//  BinarySearchLabAlgo
+//
+//  Created by Sabal on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BinarySearchLabAlgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
